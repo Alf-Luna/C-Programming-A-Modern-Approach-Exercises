4a276a393fc5dd50b9693f8906d5c19f4f2503e3
Alfo <alf.luna@duocuc.cl>
2025-08-27 23:56:55 -0400

    Chapter 2 third exercise

---
+++ Ex_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//Prints uninitialized floats and ints as a test, to look for a pattern as instructed
+
+int main(void)
+{
+    int one, two, tree, four;
+    float fone, ftwo, ftree, ffour;
+
+    printf("ints: ");
+    printf("%d\n", one);
+    printf("%d\n", two);
+    printf("%d\n", tree);
+    printf("%d\n", four);
+
+    printf("\nfloats:");
+    printf("%f\n", fone);
+    printf("%f\n", ftwo);
+    printf("%f\n", ftree);
+    printf("%f\n", ffour);
+}
